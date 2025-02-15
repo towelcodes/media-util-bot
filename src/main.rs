25ba93d6e0863d5b4f6ceb2534461019a24922aa
towelsh <tea@towel.sh>
2025-02-15 18:52:17 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,115 @@
+mod util;
+
+use std::env;
+use tracing::{debug, error, info, trace};
+use tracing_subscriber::{EnvFilter, fmt, prelude::*};
+use serenity::prelude::*;
+use serenity::{async_trait, Client};
+use serenity::all::{CommandOptionType, CreateCommand, InstallationContext, Interaction, InteractionContext};
+use serenity::builder::{CreateAttachment, CreateCommandOption, CreateInteractionResponse, CreateInteractionResponseFollowup, CreateInteractionResponseMessage};
+use serenity::model::application::{Command, ResolvedOption, ResolvedValue};
+use serenity::model::gateway::Ready;
+use tracing_subscriber::layer::SubscriberExt;
+
+struct Handler;
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        info!("{} is connected!", ready.user.name);
+
+        let commands = vec![
+            CreateCommand::new("ping").description("ping pong")
+                .integration_types(vec![InstallationContext::User])
+                .contexts(vec![InteractionContext::PrivateChannel, InteractionContext::Guild, InteractionContext::BotDm]),
+            CreateCommand::new("cake").description("i will bake you a cake !!!!"),
+            CreateCommand::new("crush").description("crushes the bit depth of an uploaded image or audio")
+                .add_option(CreateCommandOption::new(CommandOptionType::Attachment, "file", "the file you want to crush").required(true))
+                .add_option(CreateCommandOption::new(CommandOptionType::Integer, "percentage", "the percentage of the bit depth to crush").required(false))
+                .integration_types(vec![InstallationContext::User])
+                .contexts(vec![InteractionContext::PrivateChannel, InteractionContext::Guild, InteractionContext::BotDm]),
+            CreateCommand::new("compress").description("applies jpeg compression at your specified level")
+                .add_option(CreateCommandOption::new(CommandOptionType::Attachment, "file", "the file you want to compress").required(true))
+                .add_option(CreateCommandOption::new(CommandOptionType::Integer, "quality", "the quality of the compression (100 best, 0 worst)").required(false))
+                .integration_types(vec![InstallationContext::User])
+                .contexts(vec![InteractionContext::PrivateChannel, InteractionContext::Guild, InteractionContext::BotDm]),
+        ];
+        let _ = Command::set_global_commands(&ctx.http, commands).await;
+
+        debug!("commands registered");
+    }
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::Command(command) = interaction {
+            debug!("received command: {command:#?}");
+
+            let name = command.data.name.as_str();
+            if name == "crush" || name == "compress" {
+                // let file = command.data.options.get(0).unwrap().value.as_ref().unwrap().as_str().unwrap();
+                if let Some(ResolvedOption {
+                    value: ResolvedValue::Attachment(attachment), ..
+                }) = command.data.options().get(0) {
+                    debug!("performing operation on {attachment:#?}");
+                    let data = CreateInteractionResponseMessage::new().content("image processing...");
+                    let builder = CreateInteractionResponse::Message(data);
+                    if let Err(why) = command.create_response(&ctx.http, builder).await {
+                        error!("error responding: {why}");
+                        return;
+                    }
+                    
+                    debug!("filetype is {:?}", attachment.content_type);
+                    let file = attachment.download().await;
+                    if let Err(why) = file {
+                        error!("error downloading file: {why}");
+                        let builder = CreateInteractionResponseFollowup::new().content("failed to obtain file");
+                        let _ = command.create_followup(&ctx.http, builder).await;
+                        return;
+                    }
+
+                    let file = file.unwrap();
+                    let processed = match name {
+                        "crush" => util::crush(file, command.data.options.get(1).map_or_else(|| 10, |o| o.value.as_i64().unwrap())),
+                        _ => util::compress(file, command.data.options.get(1).map_or_else(|| 50, |o| o.value.as_i64().unwrap() as u8)),
+                    };
+                    if let Err(why) = processed {
+                        error!("error processing file: {why}");
+                        let builder = CreateInteractionResponseFollowup::new().content("failed to process file");
+                        let _ = command.create_followup(&ctx.http, builder).await;
+                        return;
+                    }
+
+                    let builder = CreateInteractionResponseFollowup::new()
+                        .add_file(CreateAttachment::bytes(processed.unwrap(), format!("output.{}", match name { "crush" => "png", _ => "jpg" })))
+                        .content("here's your stupid image back");
+                    if let Err(why) = command.create_followup(&ctx.http, builder).await {
+                        error!("error responding: {why}");
+                    }
+                }
+                return;
+            }
+
+            let data = CreateInteractionResponseMessage::new().content("hey loser");
+            let builder = CreateInteractionResponse::Message(data);
+            if let Err(why) = command.create_response(&ctx.http, builder).await {
+                error!("error responding: {why}");
+            }
+        }
+    }
+}
+
+#[dotenvy::load]
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::builder().parse(env::var("RUST_LOG").unwrap_or("stupid_media_bot=info".into())).unwrap())
+        .init();
+    let token = env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN must be set");
+
+    let mut client = Client::builder(token, GatewayIntents::empty())
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        error!("client error: {why:?}");
+    }
+}
---
+++ src/util.rs
@@ -0,0 +1,39 @@
+use std::io::Cursor;
+use image::{DynamicImage, ImageReader};
+use image::codecs::jpeg::JpegEncoder;
+use image::codecs::png::PngEncoder;
+use tracing::log::debug;
+
+pub fn crush(bytes: Vec<u8>, percentage: i64) -> Result<Vec<u8>, Box<dyn std::error::Error + Send + Sync>>{
+    let depth = f32::floor(((percentage as f32 / 100.0) * 255.0)) as u16;
+    debug!("percentage: {percentage}");
+    debug!("bits: {depth}");
+    let data = Cursor::new(bytes);
+    let img = ImageReader::new(data).with_guessed_format()?.decode()?;
+    let mut rgba = img.to_rgba8();
+    // let levels = (1 << depth) - 1;
+
+    for pixel in rgba.pixels_mut() {
+        for i in 0..3 { // iterate through channels
+            let chan = pixel[i] as u16;
+            pixel[i] = ((chan * depth / 255) * (255 / depth)) as u8;
+        }
+    }
+
+    let img = DynamicImage::from(rgba);
+    let mut buf: Vec<u8> = Vec::new();
+    let cursor = Cursor::new(&mut buf);
+    let encoder = PngEncoder::new(cursor);
+    img.write_with_encoder(encoder)?;
+    Ok(buf.to_owned())
+}
+
+pub fn compress(bytes: Vec<u8>, quality: u8) -> Result<Vec<u8>, Box<dyn std::error::Error + Send + Sync>> { 
+    let mut buf: Vec<u8> = Vec::new();
+    let data = Cursor::new(bytes);
+    let img = ImageReader::new(data).with_guessed_format()?.decode()?;
+    
+    let encoder = JpegEncoder::new_with_quality(&mut buf, quality); 
+    img.write_with_encoder(encoder)?;
+    Ok(buf)
+}
