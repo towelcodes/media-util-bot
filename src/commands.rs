6743214ce0852ede55fae0298fd677294e68f60b
Teatowel <tea@towel.codes>
2025-02-23 01:24:39 +0000

    default to 3 bits instead of 8 for bit crush

--- src/commands.rs
+++ src/commands.rs
@@ -12,7 +12,7 @@ pub async fn crush(cache_http: Arc<Http>, command: &CommandInteraction) -> Comma
     let file = download_attachment(command.data.options().get(0)).await?;
     mark_processing(&cache_http, &command).await;
     let arg =  command.data.options.get(1).map_or_else(|| None, |o| Some(o.value.as_i64().unwrap()));
-    let (image, process) = process::crush(file, arg.unwrap_or(8) as u8)?;
+    let (image, process) = process::crush(file, arg.unwrap_or(3) as u8)?;
     let builder = CreateInteractionResponseFollowup::new()
         .add_file(CreateAttachment::bytes(image, "output.png"))
         .content(format!("-# applied `crush` ({}) | sent by {}", process, command.user.mention()));
