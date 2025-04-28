958d22fdb3e74ff965e45028858534404fb20a83
towelsh <tea@towel.sh>
2025-04-28 17:58:01 +0100

    make the /mask command retain original transparency

--- src/process.rs
+++ src/process.rs
@@ -49,7 +49,9 @@ pub fn mask(bytes: Vec<u8>, mask: Vec<u8>) -> ImageResult {
     img.enumerate_pixels_mut().for_each(|(x, y, p)| {
         let alpha = &mut p.channels_mut()[3];
         let mask = scaled_mask.get_pixel(x, y);
-        *alpha = mask[0];
+        if *alpha > mask[0] {
+            *alpha = mask[0];
+        }
     });
     
     let mut buf: Vec<u8> = vec![];
